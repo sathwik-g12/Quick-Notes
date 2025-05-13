54ef12d9c2a0b0c3fecda2a8668b1abd8933cd98
sathwik-g12 <julurisathwik1616895@gmail.com>
2025-05-13 14:46:11 +0530

    first commit

---
+++ backend/routes/note.route.js
@@ -0,0 +1,15 @@
+import express from "express"
+import {verifyToken} from "../utils/verifyUser.js"
+import { addNote, deleteNote, editNote, getAllNotes, updateNotePinned, searchNote} from "../controller/note.controller.js"
+
+
+const router = express.Router()
+
+router.post("/add", verifyToken, addNote)
+router.post("/edit/:noteId", verifyToken, editNote)
+router.get("/all", verifyToken, getAllNotes)
+router.delete("/delete/:noteId", verifyToken, deleteNote)
+router.put("/update-note-pinned/:noteId", verifyToken, updateNotePinned)
+router.get("/search", verifyToken, searchNote)
+
+export default router
